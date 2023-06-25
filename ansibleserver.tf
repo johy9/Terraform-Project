1bf19646ab64d766cd86d437be799f6ee51d17f9
johy9 <joysiki93@gmail.com>
2023-06-25 14:13:45 -0400

    newfile

--- ansibleserver.tf
+++ ansibleserver.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "ans-n1" {
   ami           = "ami-002070d43b0a4f171"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.food-pub-sn.id
-  key_name = "greenkey"
+  key_name = "DevKey"
   vpc_security_group_ids = [aws_security_group.allow_foodapp.id]
   private_ip = "10.0.1.22"
 
