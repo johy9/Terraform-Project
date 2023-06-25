00145a6c3cf53a2dc344e71d88a32eef6cf34766
johy9 <joysiki93@gmail.com>
2023-06-25 13:41:57 -0400

    New file

---
+++ Firewall.tf
@@ -0,0 +1,32 @@
+# SECURITY GROUP
+resource "aws_security_group" "allow_food" {
+  name        = "foodapp ssh-http"
+  description = "Allow ssh & http inbound traffic"
+  vpc_id      = aws_vpc.foodapp-vpc.id
+
+  ingress {
+    description      = "ssh from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "http from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "food SG"
+  }
+}
