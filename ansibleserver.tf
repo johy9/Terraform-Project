42c44c3c3aa896c15afaa81ca39bec60856b9a04
johy9 <joysiki93@gmail.com>
2023-06-25 15:22:59 -0400

    upd

--- ansibleserver.tf
+++ ansibleserver.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ansible-master" {
   ami           = "ami-002070d43b0a4f171"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.food-pub-sn.id
-  key_name = "DevKey"
+  key_name = "devKey"
   vpc_security_group_ids = [aws_security_group.allow_food.id]
   user_data = file("install-ansible.sh")
   private_ip = "10.0.1.20"
@@ -18,7 +18,7 @@ resource "aws_instance" "ans-n1" {
   ami           = "ami-002070d43b0a4f171"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.food-pub-sn.id
-  key_name = "DevKey"
+  key_name = "devKey"
   vpc_security_group_ids = [aws_security_group.allow_food.id]
   private_ip = "10.0.1.22"
 
